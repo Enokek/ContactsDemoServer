ce19c0dc37e999eb0d639babdae0071c70a2e09f
williampettersson <william.c.pettersson@gmail.com>
2023-01-30 11:45:25 +0100

    created contacts

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5500;
+const contacts = [];
+
+contacts.push({
+  id: "1",
+  name: "Lars Larsson",
+  phone: "+46701234567",
+});
 
 app.get("/", (req, res) => {
   res.send("Test, I am alive!!!");
