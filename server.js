250b416e458af36ae8f660b4df11c351fa91ac77
williampettersson <william.c.pettersson@gmail.com>
2023-01-30 12:23:35 +0100

    contact route

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
   res.send("Test, I am alive!!!");
 });
 
+app.get("/contact/:id", (req, res) => {
+  const found = contacts.find((contact) => contact.id === req.params.id);
+
+  if (!found) {
+    res.status(404).json({ id: req.params.id, text: "Contact not found" });
+    return;
+  }
+
+  res.json(found);
+});
+
 app.listen(port, () => {
   console.log("Listening to port 5500...");
 });
